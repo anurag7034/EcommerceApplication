252b80652790c38ebb149ef7ada419d8e6e3c5bc
unknown <anuragleo1673@gmail.com>
2023-06-06 12:51:30 +0530

    initial commit

---
+++ storeapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from storeapp.models import Category,Products,Offers
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(Offers)
+
+
+
+# Register your models here.
